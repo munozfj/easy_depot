21b516c04c82a90938fba4472b4946282b86310a
Fernando J. Muñoz <munozfj@hotmail.com>
2012-09-22 11:18:57 -0300

    Inicio de aplicacion Easy Depot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyDepot::Application.config.secret_token = '626dcfd00537bde2a8598e21e12e7affa90c9244de38e6247e16c2f9c2bfcbbf37b8ac91ebcbfa7107b0b1fd4c5f5d8906dd74358f34f1165ccf9c7bc4331513'
