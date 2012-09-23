86bb410b9b1b2b6613cfde3cd5ee4728b3f437c3
Fernando J. Muñoz <munozfj@hotmail.com>
2012-09-23 11:24:34 -0300

    Validaciones

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
 
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   #Clause de order by para las consultas
   default_scope :order => 'title'
 
@@ -14,5 +17,15 @@ class Product < ActiveRecord::Base
   validates :image_url, :format => { :with => %r{\.(gif|jpg|png)$}i,
                                      :message => 'must be a URL for GIF, JPG or PNG image.'
                                    }
+
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors[:base] << "Line Items present"
+      return false
+    end
+  end
 
 end
