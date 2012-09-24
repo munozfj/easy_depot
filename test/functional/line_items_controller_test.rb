5f26b5a9e458e704e7e16c2d9013587ccbd9cac5
Fernando J. Muñoz <munozfj@hotmail.com>
2012-09-24 11:23:11 -0300

    Incorporacion de Ajax en el carrito de compras

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -20,7 +20,7 @@ class LineItemsControllerTest < ActionController::TestCase
     assert_difference('LineItem.count' ) do
       post :create, :product_id => products(:ruby).id
     end
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_redirected_to store_path 
   end
 
 
@@ -46,4 +46,6 @@ class LineItemsControllerTest < ActionController::TestCase
 
     assert_redirected_to line_items_path
   end
+
+
 end
